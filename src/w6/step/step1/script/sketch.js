6652e5fd1fa534a75318412fed52a9c6f4c8731b
soojeong123 <126849562+soojeong123@users.noreply.github.com>
2023-10-23 16:23:37 +0900

    7wClass

--- src/w6/step/step1/script/sketch.js
+++ src/w6/step/step1/script/sketch.js
@@ -1,23 +1,22 @@
-let aVariable = 20;
-let anArray = [30, 60, 90];
-let anotherArray = [];
+let particle;
+let gravity = 0;
 
 function setup() {
   setCanvasContainer('canvas', 3, 2, true);
-  console.log(aVariable);
-  console.log(anArray);
-  console.log('anArray[0]', anArray[0]);
-  console.log('anArray[1]', anArray[1]);
-  console.log('anArray[2]', anArray[2]);
-  console.log('anArray.length', anArray.length);
-  console.log('anotherArray.length', anotherArray.length);
-  console.log('anotherArray[0]', anotherArray[0]);
-  anotherArray.push('데이터');
-  console.log('anotherArray[0]', anotherArray[0]);
-  anotherArray.push(50);
-  console.log('anotherArray[1]', anotherArray[1]);
 
-  background('white');
+  particle = new Particle(width / 2, 20);
+  gravity = createVector(0, 0.1);
+
+  background(255);
 }
 
-function draw() {}
+function draw() {
+  //   console.log(particle.isDead());
+  if (particle.isDead()) {
+    particle = new Particle(width / 2, 20);
+  }
+  particle.applyForce(gravity);
+  particle.update();
+  background(255);
+  particle.display();
+}
