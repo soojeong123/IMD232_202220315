92a28e68b4974541cf00dc19c96e23003dd688e5
soojeong123 <126849562+soojeong123@users.noreply.github.com>
2023-11-06 17:58:03 +0900

    231106

--- src/w7/assignment/step1/script/Traffic.js
+++ src/w7/assignment/step1/script/Traffic.js
@@ -1,7 +1,7 @@
 class Traffic {
   //clss Traffic 설정
   constructor() {
-    //설정
+    //객체 생성
     this.vehicles = [];
     //vechicles에 array 생성
   }
@@ -9,18 +9,30 @@ class Traffic {
   run() {
     //실행
     this.vehicles.forEach((eachVehicle) => {
+      //vehicles의 각 요소들을 반복하며 (eachVehicle)을 실행
       const separate = eachVehicle.separate(this.vehicles);
+      //separate = 각 요소들의 거리를 계산
       separate.mult(1);
+      //separate에 곱하기 1
       eachVehicle.applyForce(separate);
+      //eachVehicle에 separate를 받은 applyForce를 실행
       const align = eachVehicle.align(this.vehicles);
+      //align = vehicles의 값을 eachVehicle에 전달
       align.mult(0.5);
+      //align 값에 곱하기 0.5
       eachVehicle.applyForce(align);
+      //eachVehicle에 align 값을 받은 applyForce를 실행
       const cohesion = eachVehicle.cohesion(this.vehicles);
       cohesion.mult(0.5);
+      //cohesion 값에 곱하기 0.5
       eachVehicle.applyForce(cohesion);
+      //eachVeicle에 cohesion 값을 받은 applyForce를 실행
       eachVehicle.update();
+      //eachVehicle를 업데이트
       eachVehicle.borderInfinite();
+      //eachVehicle의 borderInfinite 불러오기
       eachVehicle.display();
+      //eachVehicle를 화면에 표시
     });
   }
 
@@ -28,9 +40,9 @@ class Traffic {
     const mass = 1;
     //질량 1로 설정
     this.vehicles.push(
-      //vehicle에 아래 내용 저장
+      //vehicle에 아래 내용 입력
       new Vehicle(x, y, mass, mass * 12, 5, 0.1, color(random(360), 100, 40))
-      //새로운 vehicle에 괄호 안 내용을 추가 (x,y,질량,질량*12,5,0.1,랜덤색상)
+      //새로운 vehicle에 (x, y, mass, rad, speedMx, forceMx, color) 값을 입력
     );
   }
 }
--- src/w7/assignment/step1/script/sketch.js
+++ src/w7/assignment/step1/script/sketch.js
@@ -1,24 +1,33 @@
-let vehicle;
-let mVec;
-let debug = true;
+let traffic;
+//traffic 변수 선언
+let infiniteOffset = 80;
+//변수 infiniteOffset 선언, 값을 80으로
 
 function setup() {
-  setCanvasContainer('canvas', 2, 1, true);
-
+  setCanvasContainer('canvas', 3, 2, true);
+  //캔버스 생성, 3:2 비율로 지정
   colorMode(HSL, 360, 100, 100, 100);
-  vehicle = new Vehicle(width / 2, height / 2, 16, 5, 0.1, color(330, 100, 50));
-  mVec = createVector();
-
-  colorMode(RGB, 255, 255, 255);
-  background(255);
+  //컬러모드 HSL로 설정
+  background(30);
+  //배경색 지정
+  traffic = new Traffic(); //
+  for (let n = 0; n < 10; n++) {
+    //n의 초깃값 = 0, n < 10이 될 때까지 n에 1씩 더하기
+    traffic.addVehicle(random(width), random(height));
+    //랜덤한 위치에 Vehicle 생성, traffic에 추가
+  }
 }
 
 function draw() {
-  background(255);
-  mVec.set(mouseX, mouseY);
-  vehicle.seek(mVec);
-  vehicle.update();
-  vehicle.display();
+  //반복
+  background(30);
+  // 배경색 지정
+  traffic.run();
+  //traffic의 run함수 호출
 }
 
-//
+function mouseDragged() {
+  //마우스가 드래그 됐을때 실행
+  traffic.addVehicle(mouseX, mouseY);
+  //traffic에 새로운 Vehicle을 생성, 위치는 마우스의 위치
+}
